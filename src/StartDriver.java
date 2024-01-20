0a6556a3a84b6859232a2af0d4f8e0c5c00fd268
mhasan8801 <hasanmuhammad197@gmail.com>
2024-01-20 15:56:43 +0700

    add manual libraries and desired capabilities

--- src/StartDriver.java
+++ src/StartDriver.java
@@ -1,2 +1,9 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 public class StartDriver {
+
+    public static void main(String[] args) {
+        DesiredCapabilities cabs = new DesiredCapabilities();
+    }
+
 }
